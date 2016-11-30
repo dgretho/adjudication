ee10f53ce41a9c1d2a73209ce9c45f79b127c232
David Thomas <dgretho@gmail.com>
2016-11-30 16:20:55 +0000

    Display case data in a table

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
+/* global fetch */
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import Hello from './hello';
 
 class App extends React.Component {
-  render () {
-    return <Hello/>;
-  }
+    constructor() {
+        super();
+        
+        var self = this;
+        self.state = {
+            cases: []
+        }
+        
+        fetch('/cases')
+            .then(function(response) {
+                if(response.ok) {
+                    return response.json();    
+                } else {
+                    console.log('Network response not ok');
+                }
+            })
+            .then(function(cases) {
+                self.setState({
+                    cases: cases
+                });
+            })
+            .catch(function(error) {
+                console.log('Error occurred: ' + error);
+            })
+            
+    }
+
+    render() {
+        return <Hello cases={this.state.cases}/>;
+    }
 }
 
 render(<App/>, document.getElementById('app'));
--- src/hello.js
+++ src/hello.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {PageHeader} from 'react-bootstrap'
+import { render } from 'react-dom';
+import { Table } from 'react-bootstrap'
 
-class Hello extends React.Component {
-  render () {
-    return (<PageHeader>Hello World!</PageHeader>);
-  }
+function CaseTableRow(props) {
+    return (
+        <tr>
+            <td>{props.case.address}</td>
+            <td>{props.case.depositAmount}</td>
+        </tr>
+    )
 }
 
-export default Hello;
+function CaseTable(props) {
+    var rows = props.cases.map(function(step) {
+        return (<CaseTableRow case={step} key={step.address} />); /* TODO: Come up with better key than address */
+    });
+    
+    return (
+        <Table striped bordered condensed hover>
+            <thead>
+                <tr>
+                    <th>Address</th>
+                    <th>Deposit Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </Table>
+    );
+}
+
+export default CaseTable;
