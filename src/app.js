9534c662ae6f729eb876964bb9c4d49698c51cea
David Thomas <dgretho@gmail.com>
2016-11-30 16:27:58 +0000

    Rename Hello to CaseTable in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 /* global fetch */
 import React from 'react';
 import { render } from 'react-dom';
-import Hello from './hello';
+import CaseTable from './caseTable';
 
 class App extends React.Component {
     constructor() {
@@ -32,7 +32,7 @@ class App extends React.Component {
     }
 
     render() {
-        return <Hello cases={this.state.cases}/>;
+        return <CaseTable cases={this.state.cases}/>;
     }
 }
 
