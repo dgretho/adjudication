278d531819eb8343eea86702cf94b7eb2d5b725c
David Thomas <dgretho@gmail.com>
2016-12-02 11:53:40 +0000

    Allow the user to view if a case has evidence

--- src/caseTable.js
+++ src/caseTable.js
@@ -3,10 +3,26 @@ import { render } from 'react-dom';
 import { Table } from 'react-bootstrap'
 
 function CaseTableRow(props) {
+    function displayEvidence(landlordEvidence, tenantEvidence) {
+        var landlordHasEvidence = landlordEvidence && landlordEvidence.length > 0;
+        var tenantHasEvidence = tenantEvidence && tenantEvidence.length > 0;
+        
+        if(!landlordHasEvidence && !tenantHasEvidence) {
+            return "No";
+        } else if(landlordHasEvidence && !tenantHasEvidence) {
+            return "Landlord Only";
+        } else if(!landlordHasEvidence && tenantHasEvidence) {
+            return "Tenant Only";
+        } else {
+            return "Yes";
+        }
+    }
+    
     return (
         <tr>
             <td>{props.case.address}</td>
             <td>{props.case.depositAmount}</td>
+            <td>{displayEvidence(props.case.landlordEvidence, props.case.tenantEvidence)}</td>
         </tr>
     )
 }
@@ -22,6 +38,7 @@ function CaseTable(props) {
                 <tr>
                     <th>Address</th>
                     <th>Deposit Amount</th>
+                    <th>Has Evidence</th>
                 </tr>
             </thead>
             <tbody>
